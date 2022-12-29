31c2d8fce59b7814c5afb158c042a2c2576ccfe1
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-12-29 14:21:35 +0530

    Add files via upload

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,13 +1,17 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel') 
 
+
+//adding active class to active div
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
+        removeActiveClasses();
         panel.classList.add('active')
     })
 })
 
-function removeActiveClasses() {
+//removing active class
+
+function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
